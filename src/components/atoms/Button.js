95a929521ebcf0c1b913dab7ddfb75f0cea64866
Diogo Paes <Diogo.1hotmail.com>
2023-06-07 22:07:52 +0100

    feat:finish login screen UI

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,17 @@
+import { Button as ChakraButton } from '@chakra-ui/react'
+
+export const Button = ({ children, ...props }) => (
+  <ChakraButton
+    fontSize="16px"
+    fontWeight="bold"
+    borderRadius="16px"
+    h="56px"
+    bg="brand.primary"
+    _hover={{
+      bg: 'brand.primary'
+    }}
+    {...props}
+  >
+    {children}
+  </ChakraButton>
+)
